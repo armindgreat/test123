57f067ed59b15b2bffd47a00d51a640fd1e81887
armindgreat <malayzalawadia2003@gmail.com>
2024-08-20 11:21:34 +0530

    first commit

---
+++ Week 2/index.js
@@ -0,0 +1,16 @@
+const { Console } = require("console");
+const fs = require("fs");
+
+function print(err, data) {
+    if(err) {
+        console.log("File not found !!");
+    }
+    else {
+        console.log(data);
+    }
+}
+
+fs.readFile("a.txt", "utf-8", print); // asynchronously
+fs.readFile("b.txt", "utf-8", print);
+
+console.log("Done !!");
